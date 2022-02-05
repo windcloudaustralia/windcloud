88b3f5160d299e35262fc0bfb0601a39d11ee21e
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-05 20:26:50 +1100

    a

--- login/session.php
+++ login/session.php
@@ -4,7 +4,7 @@
 session start();
 // if the user is already logged in then redirect user to welcome page
 if (isset ($_SESSION["userid"]) && $_SESSION["userid"] === true) {
-    header ("location: /archive/index.html");
+    header ("location: welcome.php");
     exit;
 }
 ?>
