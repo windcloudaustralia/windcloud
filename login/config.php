49e67ad8a7fbbd24674e7dd3d8a3c8fa17fd3214
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-05 20:34:47 +1100

    Update config.php

--- login/config.php
+++ login/config.php
@@ -2,7 +2,7 @@
 define('DBSERVER', 'localhost'); // Database server
 define('DBUSERNAME', 'u210039736_admin'); // Database username
 define('DBPASSWORD', 'WindCloudAusPTYLTD_MySQL_auth01!$'); // Database password
-define('DBNAME', 'user_accounts'); // Database name
+define('DBNAME', 'u210039736_user_accounts'); // Database name
 
 /* connect to MySQL database */
 $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
