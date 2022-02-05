4a5908dd7b1bc52450248d99553222e69902077b
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-05 20:32:42 +1100

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,3 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Login - </title>
+        <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Login</h2>
+                    <p>Please fill in your email and password. </p>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label>Email Address</label>
+                            <input type="email" name="email" class="form-control" required />
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+                        </div>
+                        <p>Don't have an account? <a href="register.php">Register here</a>. </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+<html>
+
 <?php
 require once "config.php";
 require once "session.php";
@@ -62,37 +97,3 @@ if ($_SERVER["REQUEST METHOD"] == "POST" && isset($_ POST['submit')) {
 
 
 
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Login - </title>
-        <link rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2>Login</h2>
-                    <p>Please fill in your email and password. </p>
-                    <form action="" method="post">
-                        <div class="form-group">
-                            <label>Email Address</label>
-                            <input type="email" name="email" class="form-control" required />
-                        </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" name="password" class="form-control" required />
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
-                        </div>
-                        <p>Don't have an account? <a href="register.php">Register here</a>. </p>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </body>
-<html>
