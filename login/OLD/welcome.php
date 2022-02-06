6448b970f06d3925b0c103d396fe057cf16d26f9
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-06 15:51:09 +1100

    Update welcome.php

--- login/OLD/welcome.php
+++ login/OLD/welcome.php
@@ -4,7 +4,7 @@ session_start();
 
 // check if the user is not logged in, then redirect the user to the login page
 if (!isset($_SESSION["userid"]) || $_SESSION["userid"] !== true) {
-    header("location: login.php");
+    
     exit;
 }
 
