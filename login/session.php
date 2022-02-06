c5691d674b5fa4f11355f864c583a6b8c0cf953f
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-06 14:14:57 +1100

    Update session.php

--- login/session.php
+++ login/session.php
@@ -1,7 +1,7 @@
 <?php
 // Start the session
-
 session start();
+
 // if the user is already logged in then redirect user to welcome page
 if (isset ($_SESSION["userid"]) && $_SESSION["userid"] === true) {
     header ("location: welcome.php");
