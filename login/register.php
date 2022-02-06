d4b53fe6bc34975cf8f6f2c7d3b3a4feb4e2d0b3
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-06 14:18:22 +1100

    Update register.php

--- login/register.php
+++ login/register.php
@@ -6,7 +6,7 @@ require_once "session.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     
     $fullname = trim($_POST['name'])
-    $email = trim($_POST['email'])
+    $email = trim($_POST['email']);
     $password = trim($_POST['password'])
     $confirm_password = trim($_POST['confirm_password'])
     $password_hash = password_hash($password, PASSWORD_BCRYPT);
