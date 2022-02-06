5b7d12e08c346529fe9a786f6c671344a8dcedae
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-06 16:48:51 +1100

    Update home.php

--- login/NEW/phplogin/home.php
+++ login/NEW/phplogin/home.php
@@ -13,11 +13,11 @@ if (!isset($_SESSION['loggedin'])) {
 	<head>
 		<meta charset="utf-8">
 		<title>Home Page</title>
+		<link href="styles.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
         <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
 
 	</head>
-    
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
