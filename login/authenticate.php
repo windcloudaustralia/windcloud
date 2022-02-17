6a224c5ade30bb225ab0f13577efccbdfa8baaa0
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-02-18 10:23:19 +1100

    Update authenticate.php

--- login/authenticate.php
+++ login/authenticate.php
@@ -43,7 +43,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
             header('Location: home.php');
         } else {
             // Incorrect password
-            echo '<p style="background-color: rgb(32,30,30); font-size: 20px;">Incorrect username and/or password! <br> <br> <a href="/login/">Try again</a></p><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+            echo '<p style="background-color: rgb(32,30,30); font-size: 20px;">Incorrect username and/or password! <br> <br> <a href="/login/">Try again</a><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></p>';
             
         }
     } else {
