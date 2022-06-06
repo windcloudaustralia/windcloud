e7271ebad29c199f1517d5f791edc69bf4837c24
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-06-06 13:26:36 +1000

    Update profile.php

--- login/profile.php
+++ login/profile.php
@@ -27,43 +27,46 @@ $stmt->close();
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Wind Cloud - Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Wind Cloud</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Profile Page</h2>
-			<div>
-				<p>Your account details are below:</p>
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><?=$_SESSION['name']?></td>
-					</tr>
-					<tr>
-						<td>Password:</td>
-						<td><?=$password?></td>
-					</tr>
-					<tr>
-						<td>Email:</td>
-						<td><?=$email?></td>
-					</tr>
-				</table>
-				<p>Return to home</p><a href="/"><i class="fas fa-home"></i></a>
-			</div>
-			
-			
-			
+
+<head>
+	<meta charset="utf-8">
+	<title>Wind Cloud - Profile Page</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+
+<body class="loggedin">
+	<nav class="navtop">
+		<div>
+			<h1>Wind Cloud</h1>
+			<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+			<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 		</div>
-	</body>
+	</nav>
+	
+	<div class="content">
+		<h2>Profile Page</h2>
+		<div>
+			<p>Your account details are below:</p>
+			<table>
+				<tr>
+					<td>Username:</td>
+					<td><?= $_SESSION['name'] ?></td>
+				</tr>
+				<tr>
+					<td>Password:</td>
+					<td><?= $password ?></td>
+				</tr>
+				<tr>
+					<td>Email:</td>
+					<td><?= $email ?></td>
+				</tr>
+			</table>
+			<p>Return to home</p><a href="/"><i class="fas fa-home"></i></a>
+		</div>
+	</div>
+
+</body>
+
+<script type="text/javascript" src="indexHTML.js"></script>
 </html>
